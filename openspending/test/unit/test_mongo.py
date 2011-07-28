f9916fa2cbd9b9882a6809243ba2143cc3afdb27
Nick Stenning <nick@whiteink.com>
2011-07-28 23:08:30 +0100

    Add test.unit.test_mongo

---
+++ openspending/test/unit/test_mongo.py
@@ -0,0 +1,6 @@
+from openspending import mongo
+from openspending.test import helpers as h
+
+def test_serverside_js_loaded():
+    h.assert_true('compute_distincts' in mongo.db.system_js.list(),
+                  "'compute_distincts' serverside JS not loaded\!")
