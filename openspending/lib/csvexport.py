094f20c6967c6ae52297cfa541ab3961f6bd7e6d
Friedrich Lindenberg <friedrich@pudo.org>
2011-10-02 17:36:02 +0200

    remove chunked encoding after being explained what that means by @nickstenning, see: https://gist.github.com/1257451

--- openspending/lib/csvexport.py
+++ openspending/lib/csvexport.py
@@ -7,7 +7,7 @@ from openspending.lib.util import flatten
 
 def write_csv(entries, response):
     response.content_type = 'text/csv'
-    response.headers['Transfer-Encoding'] = 'chunked'
+    #response.headers['Transfer-Encoding'] = 'chunked'
     return generate_csv(entries)
 
 def generate_csv(entries):
