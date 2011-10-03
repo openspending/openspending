0ad30868d6eb27fd27960c3fef42308e485c9f25
Friedrich Lindenberg <friedrich@pudo.org>
2011-10-03 19:23:02 +0200

    begin to rebuild a bit more.

--- openspending/model/meta.py
+++ openspending/model/meta.py
@@ -4,6 +4,10 @@ from sqlalchemy import MetaData
 from sqlalchemy import Table, Column, ForeignKey, Integer, Boolean
 from sqlalchemy import or_, and_
 
+from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
+from sqlalchemy import orm
+
+
 # SQLAlchemy database engine.  Updated by model.init_model()
 engine = None
 
@@ -13,3 +17,12 @@ session = None
 # Global metadata. If you have multiple databases with overlapping table
 # names, you'll need a metadata for each database
 metadata = MetaData()
+
+
+class Model(object):
+    """Baseclass for custom user models."""
+
+    query_class = orm.Query
+    query = None
+
+Model = declarative_base(cls=Model, name='Model')
