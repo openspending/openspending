263df67722a320d8a45c79ccfded35761b5d5000
Friedrich Lindenberg <friedrich@pudo.org>
2011-10-05 16:50:06 +0200

    reproduce test dataset which was a binary pickle before.

--- openspending/test/helpers.py
+++ openspending/test/helpers.py
@@ -3,8 +3,9 @@ from nose.plugins.skip import SkipTest
 from mock import Mock, patch, MagicMock
 
 import os as _os
+import csv
 
-from openspending import model as _model
+from openspending.model import Dataset, meta as db
 from openspending.lib import solr_util as _solr
 
 TEST_ROOT = _os.path.dirname(__file__)
@@ -13,7 +14,19 @@ def load_fixture(name):
     """
     Load fixture data into the database.
     """
-    _pymongodump.restore(_mongo.db, fixture_path('%s.pickle' % name), drop=False)
+    import json
+    fh = open(fixture_path('%s.js' % name), 'r')
+    data = json.load(fh)
+    fh.close()
+    dataset = Dataset(data)
+    db.session.add(db)
+    db.session.commit()
+    dataset.generate()
+    fh = open(fixture_path('%s.csv' % name), 'r')
+    reader = csv.DictReader(fh)
+    dataset.load_all(reader)
+    fh.close()
+    return dataset
 
 def fixture_file(name):
     """Return a file-like object pointing to a named fixture."""
@@ -32,8 +45,8 @@ def clean_all():
     clean_solr()
 
 def clean_db():
-    _model.meta.session.rollback()
-    _model.meta.metadata.drop_all()
+    db.session.rollback()
+    db.metadata.drop_all()
 
 def clean_solr():
     '''Clean all entries from Solr.'''
@@ -44,9 +57,8 @@ def clean_solr():
 def clean_and_reindex_solr():
     '''Clean Solr and reindex all entries in the database.'''
     clean_solr()
-    dataset_names = _model.dataset.distinct('name')
-    for name in dataset_names:
-        _solr.build_index(name)
+    for dataset in db.session.query(Dataset):
+        _solr.build_index(dataset.name)
 
 def skip_if_stubbed_solr():
     if type(_solr.get_connection()) == _solr._Stub:
