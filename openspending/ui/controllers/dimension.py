10d27abd47a071b6968087b1a9eeeead6583a493
Friedrich Lindenberg <friedrich@pudo.org>
2011-10-06 17:04:24 +0200

    dimension controller now only uses aggregation to make listings of distinct values.

--- openspending/ui/controllers/dimension.py
+++ openspending/ui/controllers/dimension.py
@@ -5,7 +5,6 @@ from pylons.controllers.util import abort
 from pylons.decorators.cache import beaker_cache
 from pylons.i18n import _
 
-from openspending.lib.cubes import find_cube
 from openspending import model
 from openspending.ui.lib.jsonp import to_jsonp
 from openspending.ui.lib.base import BaseController, render
@@ -22,13 +21,12 @@ class DimensionController(BaseController):
            cache_response=False,
            query_args=True)
     def index(self, dataset, format='html'):
-        c.dataset = model.dataset.find_one_by('name', dataset)
+        c.dataset = model.Dataset.by_name(dataset)
         if not c.dataset:
             abort(404, _('Sorry, there is no dataset named %s') % dataset)
-        c.dimensions = model.dimension.get_dataset_dimensions(c.dataset['name'])
-        c.dimensions = [d for d in c.dimensions if d['key'] not in ENTRY_FIELDS]
+        c.dimensions = c.dataset.dimensions 
         if format == 'json':
-            return to_jsonp(list(c.dimensions))
+            return to_jsonp([d.as_dict() for d in c.dimensions])
         else:
             return render('dimension/index.html')
 
@@ -36,47 +34,30 @@ class DimensionController(BaseController):
            cache_response=False,
            query_args=True)
     def view(self, dataset, dimension, format='html'):
-        c.dataset = model.dataset.find_one_by('name', dataset)
+        c.dataset = model.Dataset.by_name(dataset)
         if not c.dataset:
             abort(404, _('Sorry, there is no dataset named %s') % dataset)
-        if dimension in ENTRY_FIELDS or "." in dimension:
+        try:
+            c.dimension = c.dataset[dimension]
+        except KeyError:
             abort(400, _('This is not a dimension'))
-        c.meta = model.dimension.find_one({"dataset": c.dataset['name'],
-                                           "key": dimension})
-        if c.meta is None:
-            c.meta = {}
+        if not isinstance(c.dimension, model.Dimension):
+            abort(400, _('This is not a dimension'))
 
         # TODO: pagination!
         try:
             page = int(request.params.get('page'))
         except:
             page = 1
-        cube = find_cube(c.dataset, [dimension])
-       # ok
-        if cube is not None:
-            try:
-                result = cube.query([dimension], page=page, pagesize=PAGE_SIZE,
-                                order=[('amount', True)])
-            except Exception as e:
-                error = str(e)
-                if "too much data for sort" in error:
-                    error = """Database tuning required: the dataset specified
-                               is so large that it cannot be searched quickly
-                               enough to fulfil your request in reasonable time.
-                               Please request that an administrator add an
-                               index to speed up this query."""
-                abort(403, error)
-            items = result.get('drilldown', [])
-            c.values = [(d.get(dimension), d.get('amount')) for d in items]
-        else:
-            abort(403, "none")
-            items = distinct(dimension, dataset_name=c.dataset['name'])
-            c.values = [(i, 0.0) for i in items]
+        result = c.dataset.aggregate(drilldowns=[dimension], page=page, 
+                    pagesize=PAGE_SIZE, order=[('amount', True)])
+        items = result.get('drilldown', [])
+        c.values = [(d.get(dimension), d.get('amount')) for d in items]
 
         if format == 'json':
             return to_jsonp({
                 "values": c.values,
-                "meta": c.meta})
+                "meta": c.dimension.as_dict()})
 
         c.page = Page(c.values, page=page,
                       items_per_page=PAGE_SIZE)
