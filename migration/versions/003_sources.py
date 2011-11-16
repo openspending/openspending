8970c3a77db3de0a0036ea6e1dd53ab37d54ac00
Friedrich Lindenberg <friedrich@pudo.org>
2011-11-16 11:35:22 +0100

    fix migration to not depend on model.meta.

--- migration/versions/003_sources.py
+++ migration/versions/003_sources.py
@@ -12,12 +12,12 @@ def upgrade(migrate_engine):
     account = Table('account', meta, autoload=True)
 
     source_table = Table('source', meta,
-        db.Column('id', db.Integer, primary_key=True),
-        db.Column('url', db.Unicode),
-        db.Column('analysis', JSONType, default=dict),
-        db.Column('created_at', db.DateTime, default=datetime.utcnow),
-        db.Column('dataset_id', db.Integer, db.ForeignKey('dataset.id')),
-        db.Column('creator_id', db.Integer, db.ForeignKey('account.id'))
+        Column('id', Integer, primary_key=True),
+        Column('url', Unicode),
+        Column('analysis', Unicode, default=dict),
+        Column('created_at', DateTime, default=datetime.utcnow),
+        Column('dataset_id', Integer, ForeignKey('dataset.id')),
+        Column('creator_id', Integer, ForeignKey('account.id'))
         )
 
     source_table.create()
