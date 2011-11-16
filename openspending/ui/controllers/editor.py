bb64a3c862a27bd79eb3cf6fe5f6947a6c8f5ef8
Friedrich Lindenberg <friedrich@pudo.org>
2011-11-16 11:19:06 +0100

    clean up editor imports.

--- openspending/ui/controllers/editor.py
+++ openspending/ui/controllers/editor.py
@@ -1,16 +1,13 @@
 import logging
 
-from pylons import request, response, tmpl_context as c
-from pylons.controllers.util import redirect
+from pylons import request, tmpl_context as c
 from pylons.i18n import _
 from colander import Invalid
 
-from openspending import model
-from openspending.model import Dataset, meta as db
-from openspending.lib.jsonexport import to_jsonp
+from openspending.model import meta as db
 from openspending.ui.lib import helpers as h
 from openspending.ui.lib.base import BaseController, render
-from openspending.ui.lib.base import abort, require
+from openspending.ui.lib.base import require
 from openspending.validation.model.currency import CURRENCIES
 from openspending.validation.model.dataset import dataset_schema
 from openspending.validation.model.common import ValidationState
@@ -18,7 +15,6 @@ from openspending.validation.model.common import ValidationState
 log = logging.getLogger(__name__)
 
 class EditorController(BaseController):
-
 
     def index(self, dataset, format='html'):
         self._get_dataset(dataset)
