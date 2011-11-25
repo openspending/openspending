29b14c7fca81fb739849c4556c12b7681bc1e69b
Friedrich Lindenberg <friedrich@pudo.org>
2011-11-25 12:18:06 +0100

    [r] fix up analysis to run in reasonable time.

--- openspending/importer/analysis.py
+++ openspending/importer/analysis.py
@@ -2,8 +2,15 @@ from collections import defaultdict
 from urllib import urlopen
 
 from messytables import CSVRowSet, type_guess
+from messytables.types import TYPES, DateType
 from openspending.lib.util import slugify
 
+class LocalDateType(DateType):
+    formats = ["%Y", "%Y-%m", "%Y-%m-%d"]
+
+LIMITED_TYPES = list(TYPES)
+LIMITED_TYPES.remove(DateType)
+LIMITED_TYPES.append(LocalDateType)
 
 def frequent_values(sample):
     values = defaultdict(lambda: defaultdict(int))
@@ -16,28 +23,27 @@ def frequent_values(sample):
         sorted_values.append(frequent[:5])
     return sorted_values
 
-def analyze_csv(url, sample=2000):
+def analyze_csv(url, sample=1000):
     try:
         fileobj = urlopen(url)
         row_set = CSVRowSet('data', fileobj, window=sample)
         sample = list(row_set.sample)
         headers, sample = sample[0], sample[1:]
-        values = frequent_values(sample)
-        types = type_guess(sample)
+        #values = frequent_values(sample)
+        types = type_guess(sample[500:], types=LIMITED_TYPES)
         mapping = {}
-        for header, type_, value in zip(headers, types, values):
+        for header, type_ in zip(headers, types):
             type_ = repr(type_).lower()
             name = slugify(header.value).lower()
             meta = {
                 'label': header.value,
                 'column': header.value,
-                'common_values': value,
                 'datatype': type_
                 }
             if type_ in ['decimal', 'integer', 'float']:
                 meta['type'] = 'measure'
                 meta['datatype'] = 'float'
-            elif type_ in ['date']:
+            elif type_.startswith('date'):
                 meta['type'] = 'date'
                 meta['datatype'] = 'date'
             else:
