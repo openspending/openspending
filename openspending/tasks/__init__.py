5aaff9c0f155f67d8ec0ccc07aaf13304dbd1d5d
Friedrich Lindenberg <friedrich@pudo.org>
2011-11-25 11:35:58 +0100

    [r] implement some background processing tasks for celery - analyze is a stub for now.

--- openspending/tasks/__init__.py
+++ openspending/tasks/__init__.py
@@ -1,15 +1,42 @@
+import logging
 from celery.decorators import task
 
 import openspending.command.celery
 
+log = logging.getLogger(__name__)
+
 @task(ignore_result=True)
-def greet():
-    from openspending.model import Dataset, meta as db
-    print "Loaded datasets: "
-    for dataset in db.session.query(Dataset):
-        print ' * ' + dataset.name
+def ping():
+    log.info("Pong.")
 
 
+@task(ignore_result=True)
+def analyze_source(source_id):
+    from openspending.model import Source
+    source = Source.by_id(source_id)
+    if source is None:
+        log.error("No such source: %s", source_id)
+    log.info("Analyzing: %s", source)
 
+
+@task(ignore_result=True)
+def load_source(source_id, sample=False):
+    from openspending.model import Source
+    from openspending.importer import CSVImporter
+    source = Source.by_id(source_id)
+    if source is None:
+        log.error("No such source: %s", source_id)
+    importer = CSVImporter(source)
+    if sample:
+        importer.run(max_lines=1000, max_errors=1000)
+    else:
+        importer.run()
+        index_dataset.delay(source.dataset.name)
+
+
+@task(ignore_result=True)
+def index_dataset(dataset_name):
+    from openspending.lib.solr_util import build_index
+    build_index(dataset_name)
 
 
