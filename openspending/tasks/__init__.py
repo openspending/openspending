0af37dc618e59b3cad5eb9161b1db73bbc89b527
Friedrich Lindenberg <friedrich@pudo.org>
2011-11-28 21:16:32 +0100

    try to handle missing sources better.

--- openspending/tasks/__init__.py
+++ openspending/tasks/__init__.py
@@ -15,9 +15,9 @@ def analyze_source(source_id):
     from openspending.model import Source, meta as db
     from openspending.importer.analysis import analyze_csv
     source = Source.by_id(source_id)
-    if source is None:
+    if not source:
         log.error("No such source: %s", source_id)
-    log.info("Analyzing: %s", source)
+    log.info("Analyzing: %s", source.url)
     source.analysis = analyze_csv(source.url)
     if 'error' in source.analysis:
         log.error(source.analysis.get('error'))
@@ -30,7 +30,7 @@ def load_source(source_id, sample=False):
     from openspending.model import Source
     from openspending.importer import CSVImporter
     source = Source.by_id(source_id)
-    if source is None:
+    if not source:
         log.error("No such source: %s", source_id)
 
     if not source.loadable:
