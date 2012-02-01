b86a22c90e55daf543b0cae1b7dd2603e9b0f200
Andrew Suffield <asuffield@suffields.me.uk>
2012-02-01 14:58:42 +0000

    Kill off old /locale switch

--- openspending/ui/config/routing.py
+++ openspending/ui/config/routing.py
@@ -42,7 +42,6 @@ def make_map():
     map.connect('/', controller='home', action='index')
 
     map.connect('/getinvolved', controller='home', action='getinvolved')
-    map.connect('/locale', controller='home', action='locale')
     map.connect('/set-locale', controller='home', action='set_locale', conditions=dict(method=['POST']))
 
     map.connect('/login', controller='account', action='login')
