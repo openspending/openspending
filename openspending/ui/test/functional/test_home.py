1ab181e5547f4fc0d1754d61babd0eb345493d31
Friedrich Lindenberg <friedrich@pudo.org>
2012-02-20 18:23:32 +0100

    Set-locale only takes post.

--- openspending/ui/test/functional/test_home.py
+++ openspending/ui/test/functional/test_home.py
@@ -7,4 +7,4 @@ class TestHomeController(ControllerTestCase):
         assert 'OpenSpending' in response
 
     def test_locale(self):
-        response = self.app.get(url(controller='home', action='set_locale'))
+        response = self.app.post(url(controller='home', action='set_locale'))
