a97a6610b843c320fc829571387ef577a65ce855
Friedrich Lindenberg <friedrich@pudo.org>
2012-04-04 09:31:45 +0200

    Bugfix: re-introduce parts of the pager class to support error listings in source runs.

--- openspending/ui/controllers/run.py
+++ openspending/ui/controllers/run.py
@@ -1,17 +1,26 @@
 import logging
 
-from pylons import request, response, tmpl_context as c
-from pylons.controllers.util import redirect
+from pylons import tmpl_context as c
 from pylons.i18n import _
 
-from openspending import model
-from openspending.model import Source, Run, LogRecord, meta as db
-from openspending.ui.lib import helpers as h
-from openspending.ui.lib.page import Page
+from openspending.model import Source, Run, LogRecord
+from webhelpers import paginate
 from openspending.ui.lib.base import BaseController, render
 from openspending.ui.lib.base import abort, require
 
 log = logging.getLogger(__name__)
+
+
+class Page(paginate.Page):
+    # Curry the pager method of the webhelpers.paginate.Page class, so we have
+    # our custom layout set as default.
+    def pager(self, *args, **kwargs):
+        kwargs.update(
+            format="<div class='pager'>$link_previous ~2~ $link_next</div>",
+            symbol_previous=u'\xab Prev', symbol_next=u'Next \xbb'
+        )
+        return super(Page, self).pager(*args, **kwargs)
+
 
 class RunController(BaseController):
 
@@ -29,14 +38,12 @@ class RunController(BaseController):
         self._get_run(dataset, source, id)
         system = c.run.records.filter_by(category=LogRecord.CATEGORY_SYSTEM)
         c.num_system = system.count()
-        c.system_page = Page(system.order_by(LogRecord.timestamp.asc()), 
+        c.system_page = Page(system.order_by(LogRecord.timestamp.asc()),
                 page=self._get_page('system_page'),
                 items_per_page=10)
         data = c.run.records.filter_by(category=LogRecord.CATEGORY_DATA)
         c.num_data = data.count()
-        c.data_page = Page(data.order_by(LogRecord.timestamp.asc()), 
+        c.data_page = Page(data.order_by(LogRecord.timestamp.asc()),
                 page=self._get_page('data_page'),
                 items_per_page=20)
         return render('run/view.html')
-
-
