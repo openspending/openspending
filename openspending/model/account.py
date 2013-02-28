19be6594bab007c362e96b7eba5a179f5f3e1519
Nigel Babu <nigelbabu@gmail.com>
2013-02-28 20:59:13 +0530

    Add Boolean field in the model for terms

--- openspending/model/account.py
+++ openspending/model/account.py
@@ -30,6 +30,7 @@ class Account(db.Model):
     api_key = db.Column(db.Unicode(2000), default=make_uuid)
     admin = db.Column(db.Boolean, default=False)
     script_root = db.Column(db.Unicode(2000))
+    terms = db.Column(db.Boolean, default=False)
 
     datasets = db.relationship(Dataset,
             secondary=account_dataset_table,
