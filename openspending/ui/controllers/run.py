4c13b54baa3bcf3c32a149656e26b91867ebab3f
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2013-11-17 14:08:40 +0000

    Import templating in run controller

--- openspending/ui/controllers/run.py
+++ openspending/ui/controllers/run.py
@@ -6,7 +6,7 @@ from pylons.i18n import _
 from openspending.model import Source, Run, LogRecord
 from openspending.ui.lib.base import BaseController
 from openspending.ui.lib.base import abort, require
-from openspending.ui.alttemplates.templating import Page
+from openspending.ui.alttemplates import templating
 
 log = logging.getLogger(__name__)
 
@@ -26,12 +26,12 @@ class RunController(BaseController):
         self._get_run(dataset, source, id)
         system = c.run.records.filter_by(category=LogRecord.CATEGORY_SYSTEM)
         c.num_system = system.count()
-        c.system_page = Page(system.order_by(LogRecord.timestamp.asc()),
+        c.system_page = templating.Page(system.order_by(LogRecord.timestamp.asc()),
                 page=self._get_page('system_page'),
                 items_per_page=10)
         data = c.run.records.filter_by(category=LogRecord.CATEGORY_DATA)
         c.num_data = data.count()
-        c.data_page = Page(data.order_by(LogRecord.timestamp.asc()),
+        c.data_page = templating.Page(data.order_by(LogRecord.timestamp.asc()),
                 page=self._get_page('data_page'),
                 items_per_page=20)
         return templating.render('run/view.html')
