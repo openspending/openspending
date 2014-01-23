edcaea101c4f93391ff077446f25b37749c5c388
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-01-23 14:18:46 +0100

    Fix PEP8 warnings in auth.

--- openspending/auth/dataset.py
+++ openspending/auth/dataset.py
@@ -1,13 +1,16 @@
 from account import logged_in
 from pylons import tmpl_context
 
+
 def create():
     return logged_in()
+
 
 def read(dataset):
     if not dataset.private:
         return True
     return update(dataset)
+
 
 def update(dataset):
     if logged_in():
@@ -16,6 +19,7 @@ def update(dataset):
         elif tmpl_context.account in dataset.managers:
             return True
     return False
+
 
 def delete(dataset):
     if logged_in():
