e3b97bd198701eb67ccc85336fe1acf91e5207e9
Friedrich Lindenberg <friedrich@pudo.org>
2014-12-21 17:00:06 +0100

    Settings for OS

--- openspending/default_settings.py
+++ openspending/default_settings.py
@@ -1,7 +1,12 @@
 
+SECRET_KEY = 'foo'
 DEBUG = True
+
+SITE_TITLE = 'OpenSpending'
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/openspending'
 #SQLALCHEMY_ECHO = True
 
-SOLR_URL = 'http://localhost:8983/solr/openspending-test'
+SOLR_URL = 'http://localhost:8983/solr/openspending'
+
+BABEL_DEFAULT_LOCALE = 'en'
