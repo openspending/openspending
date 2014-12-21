fb6b67c52c09a19e46db44b4d04a54a5eb72c16c
Friedrich Lindenberg <friedrich@pudo.org>
2014-12-21 18:44:17 +0100

    Message flashing.

--- openspending/views/helpers.py
+++ openspending/views/helpers.py
@@ -1,4 +1,5 @@
 from flask import url_for as flask_url_for
+from flask import flash
 
 
 def url_for(endpoint, **kwargs):
@@ -7,3 +8,20 @@ def url_for(endpoint, **kwargs):
 
 def static_path(filename):
     return url_for('static', filename=filename)
+
+
+def disable_cache(func):
+    # TODO: set request variable
+    return func
+
+
+def flash_notice(message):
+    return flash(message, 'notice')
+
+
+def flash_error(message):
+    return flash(message, 'error')
+
+
+def flash_success(message):
+    return flash(message, 'success')
