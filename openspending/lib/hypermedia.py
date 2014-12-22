a4773e7897850c9310a7a7c0720c65ec05bbe2a7
Friedrich Lindenberg <friedrich@pudo.org>
2014-12-22 11:03:31 +0100

    Fix up “hypermedia” link generation.

--- openspending/lib/hypermedia.py
+++ openspending/lib/hypermedia.py
@@ -1,18 +1,16 @@
+from openspending.lib.helpers import url_for
 
 
 def dataset_apply_links(dataset):
-    dataset['html_url'] = url_for(controller='dataset',
-                                  action='view', dataset=dataset['name'],
-                                  qualified=True)
+    dataset['html_url'] = url_for('dataset.view', dataset=dataset['name'])
     dataset['badges'] = badges_apply_links(dataset['badges'])
     return dataset
 
 
 def entry_apply_links(dataset_name, entry):
     if isinstance(entry, dict) and 'id' in entry:
-        entry['html_url'] = url_for(controller='entry',
-                                    action='view', dataset=dataset_name,
-                                    id=entry['id'], qualified=True)
+        entry['html_url'] = url_for('entry.view', dataset=dataset_name,
+                                    id=entry['id'])
         for k, v in entry.items():
             entry[k] = member_apply_links(dataset_name, k, v)
     return entry
@@ -20,18 +18,15 @@ def entry_apply_links(dataset_name, entry):
 
 def dimension_apply_links(dataset_name, dimension):
     name = dimension.get('name', dimension.get('key'))
-    dimension['html_url'] = url_for(controller='dimension',
-                                    action='view', dataset=dataset_name,
-                                    dimension=name, qualified=True)
+    dimension['html_url'] = url_for('dimension.view', dataset=dataset_name,
+                                    dimension=name)
     return dimension
 
 
 def member_apply_links(dataset_name, dimension, data):
     if isinstance(data, dict) and 'name' in data:
-        data['html_url'] = url_for(controller='dimension',
-                                   action='member', dataset=dataset_name,
-                                   dimension=dimension, name=data['name'],
-                                   qualified=True)
+        data['html_url'] = url_for('dimension.member', dataset=dataset_name,
+                                   dimension=dimension, name=data['name'])
     return data
 
 
@@ -62,10 +57,8 @@ def badge_apply_links(badge):
     representation to include a fully qualified domain
     """
     # Add an html_url to represent the html representation of the badge
-    badge['html_url'] = url_for(controller='badge', action='information',
-                                id=badge['id'], qualified=True)
+    badge['html_url'] = url_for('badge.information', id=badge['id'])
     # Change the image url to be a fully qualified url if it isn't already
     needs_qualified = not str(badge['image']).startswith('http://')
     badge['image'] = url_for(str(badge['image']), qualified=needs_qualified)
-
     return badge
