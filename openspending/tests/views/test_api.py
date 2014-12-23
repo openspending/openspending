7b0f144178053938cd1165ca652378e9cd96f758
Friedrich Lindenberg <friedrich@pudo.org>
2014-12-23 23:15:04 +0100

    All tests for the API are green :)

--- openspending/tests/views/test_api.py
+++ openspending/tests/views/test_api.py
@@ -130,104 +130,104 @@ class TestApiController(ControllerTestCase):
                                         2005, 2004, 2003])
         assert unique(order) == expected_result
 
-    # def test_search(self):
-    #     response = self.client.get(url_for('api.search'))
-    #     result = json.loads(response.data)
+    def test_search(self):
+        response = self.client.get(url_for('api.search'))
+        result = json.loads(response.data)
 
-    #     assert result['stats']['results_count'] == 36
-    #     assert result['stats']['results_count_query'] == 36
-    #     assert result['facets'] == {}
-    #     assert len(result['results']) == 36
+        assert result['stats']['results_count'] == 36
+        assert result['stats']['results_count_query'] == 36
+        assert result['facets'] == {}
+        assert len(result['results']) == 36
 
-    # def test_search_results_dataset(self):
-    #     response = self.client.get(url_for('api.search'))
-    #     result = json.loads(response.data)
+    def test_search_results_dataset(self):
+        response = self.client.get(url_for('api.search'))
+        result = json.loads(response.data)
 
-    #     assert result['results'][0]['dataset']['name'] == 'cra'
-    #     expected_label = 'Country Regional Analysis v2009'
-    #     assert result['results'][0]['dataset']['label'] == expected_label
+        assert result['results'][0]['dataset']['name'] == 'cra'
+        expected_label = 'Country Regional Analysis v2009'
+        assert result['results'][0]['dataset']['label'] == expected_label
 
-    # def test_search_page_pagesize(self):
-    #     response = self.client.get(url_for('api.search', page=2, pagesize=10))
-    #     result = json.loads(response.data)
+    def test_search_page_pagesize(self):
+        response = self.client.get(url_for('api.search', page=2, pagesize=10))
+        result = json.loads(response.data)
 
-    #     assert result['stats']['results_count'] == 10
-    #     assert result['stats']['results_count_query'] == 36
+        assert result['stats']['results_count'] == 10
+        assert result['stats']['results_count_query'] == 36
 
-    # def test_search_q(self):
-    #     response = self.client.get(url_for('api.search',
-    #                                        q="Ministry of Justice"))
-    #     result = json.loads(response.data)
+    def test_search_q(self):
+        response = self.client.get(url_for('api.search',
+                                           q="Ministry of Justice"))
+        result = json.loads(response.data)
 
-    #     assert result['stats']['results_count'] == 5
-    #     assert result['stats']['results_count_query'] == 5
+        assert result['stats']['results_count'] == 5
+        assert result['stats']['results_count_query'] == 5
 
-    #     id_value = '06dafa7250420ab1dc616d2bbbe310c9ad6e485e'
-    #     assert result['results'][0]['id'] == id_value
+        id_value = '06dafa7250420ab1dc616d2bbbe310c9ad6e485e'
+        assert result['results'][0]['id'] == id_value
 
-    # def test_search_filter(self):
-    #     response = self.client.get(url_for('api.search',
-    #                                        filter="pog:P13 S091105"))
-    #     result = json.loads(response.data)
+    def test_search_filter(self):
+        response = self.client.get(url_for('api.search',
+                                           filter="pog:P13 S091105"))
+        result = json.loads(response.data)
 
-    #     assert result['stats']['results_count'] == 5
-    #     assert result['stats']['results_count_query'] == 5
+        assert result['stats']['results_count'] == 5
+        assert result['stats']['results_count_query'] == 5
 
-    #     id_value = '06dafa7250420ab1dc616d2bbbe310c9ad6e485e'
-    #     assert result['results'][0]['id'] == id_value
+        id_value = '06dafa7250420ab1dc616d2bbbe310c9ad6e485e'
+        assert result['results'][0]['id'] == id_value
 
-    # def test_search_facet(self):
-    #     response = self.client.get(url_for('api.search',
-    #                                        pagesize=0,
-    #                                        facet_field="dataset"))
-    #     result = json.loads(response.data)
+    def test_search_facet(self):
+        response = self.client.get(url_for('api.search',
+                                           pagesize=0,
+                                           facet_field="dataset"))
+        result = json.loads(response.data)
 
-    #     assert len(result['facets']['dataset']) == 1
-    #     assert result['facets']['dataset'][0] == ['cra', 36]
+        assert len(result['facets']['dataset']) == 1
+        assert result['facets']['dataset'][0] == ['cra', 36]
 
-    # def test_search_expand_facet_dimensions(self):
-    #     response = self.client.get(url_for('api.search',
-    #                                        dataset='cra',
-    #                                        pagesize=0,
-    #                                        facet_field="from|to.name",
-    #                                        expand_facet_dimensions="1"))
-    #     result = json.loads(response.data)
+    def test_search_expand_facet_dimensions(self):
+        response = self.client.get(url_for('api.search',
+                                           dataset='cra',
+                                           pagesize=0,
+                                           facet_field="from|to.name",
+                                           expand_facet_dimensions="1"))
+        result = json.loads(response.data)
 
-    #     hra = {
-    #         "taxonomy": "from",
-    #         "description": "",
-    #         "id": 5,
-    #         "name": "999",
-    #         "label": "ENG_HRA"}
+        hra = {
+            "taxonomy": "from",
+            "description": "",
+            "id": 5,
+            "name": "999",
+            "label": "ENG_HRA"}
 
-    #     assert result['facets']['from'][0][0] == hra
-    #     assert result['facets']['to.name'][0][0] == 'society'
+        assert result['facets']['from'][0][0] == hra
+        assert result['facets']['to.name'][0][0] == 'society'
 
-    # def test_search_expand_facet_dimensions_no_dataset(self):
-    #     response = self.client.get(url_for('api.search',
-    #                                        pagesize=0,
-    #                                        facet_field="from",
-    #                                        expand_facet_dimensions="1"))
-    #     result = json.loads(response.data)
+    def test_search_expand_facet_dimensions_no_dataset(self):
+        response = self.client.get(url_for('api.search',
+                                           pagesize=0,
+                                           facet_field="from",
+                                           expand_facet_dimensions="1"))
+        result = json.loads(response.data)
 
-    #     # facets should *NOT* be expanded unless exactly 1 dataset was
-    #     # specified
-    #     assert result['facets']['from'][0][0] == '999'
+        # facets should *NOT* be expanded unless exactly 1 dataset was
+        # specified
+        assert result['facets']['from'][0][0] == '999'
 
-    # def test_search_order(self):
-    #     response = self.client.get(url_for('api.search',
-    #                                        order="amount:asc"))
-    #     result = json.loads(response.data)
+    def test_search_order(self):
+        response = self.client.get(url_for('api.search',
+                                           order="amount:asc"))
+        result = json.loads(response.data)
 
-    #     amounts = [r['amount'] for r in result['results']]
-    #     assert amounts == sorted(amounts)
+        amounts = [r['amount'] for r in result['results']]
+        assert amounts == sorted(amounts)
 
-    #     response = self.client.get(url_for('api.search',
-    #                                        order="amount:desc"))
-    #     result = json.loads(response.data)
+        response = self.client.get(url_for('api.search',
+                                           order="amount:desc"))
+        result = json.loads(response.data)
 
-    #     amounts = [r['amount'] for r in result['results']]
-    #     assert amounts == sorted(amounts)[::-1]
+        amounts = [r['amount'] for r in result['results']]
+        assert amounts == sorted(amounts)[::-1]
 
     def test_inflation(self):
         """
@@ -418,8 +418,7 @@ class TestApiController(ControllerTestCase):
             'User can delete a nonexistent dataset'
 
 
-#class TestApiNewDataset(ControllerTestCase):
-class cccc(object):
+class TestApiNewDataset(ControllerTestCase):
 
     """
     This checks for authentication with a header api key while also
