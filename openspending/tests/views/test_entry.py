8854f853d919ec46f7e01137f6da62444e0ba98f
Friedrich Lindenberg <friedrich@pudo.org>
2014-12-23 17:16:01 +0100

    Move over REST controller tests.

--- openspending/tests/views/test_entry.py
+++ openspending/tests/views/test_entry.py
@@ -6,6 +6,35 @@ from openspending.tests.base import ControllerTestCase
 from openspending.tests.helpers import load_fixture
 
 
+class TestRestController(ControllerTestCase):
+
+    def setUp(self):
+        super(TestRestController, self).setUp()
+        load_fixture('cra')
+        self.cra = Dataset.by_name('cra')
+
+    def test_dataset(self):
+        response = self.client.get(url_for('dataset.view',
+                                    format='json',
+                                    dataset=self.cra.name))
+
+        assert '"name": "cra"' in response.data, response.data
+
+    def test_entry(self):
+        q = self.cra['from'].alias.c.name == 'Dept047'
+        example = list(self.cra.entries(q, limit=1)).pop()
+
+        response = self.client.get(url_for('entry.view',
+                                    dataset=self.cra.name,
+                                    format='json',
+                                    id=str(example['id'])))
+
+        assert '"id":' in response.data, response.data
+        assert '"cofog1":' in response.data, response.data
+        assert '"from":' in response.data, response.data
+        assert '"Dept047"' in response.data, response.data
+
+
 class TestEntryController(ControllerTestCase):
 
     def setUp(self):
