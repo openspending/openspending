6499c7838ed600aa19d4405d830ac7dcab15be14
Friedrich Lindenberg <friedrich@pudo.org>
2014-12-24 12:46:33 +0100

    Simple caching system ported from grano.

---
+++ openspending/views/cache.py
@@ -0,0 +1,70 @@
+from hashlib import sha1
+
+from flask import current_app, request, Response
+from flask.ext.babel import get_locale
+from flask.ext.login import current_user
+
+
+class NotModified(Exception):
+    pass
+
+
+def handle_not_modified(exc):
+    return Response(status=304)
+
+
+def setup_caching():
+    request._http_cache = current_app.config.get('CACHE')
+    request._http_etag = None
+
+
+def disable_cache():
+    request._http_cache = False
+
+
+def cache_response(resp):
+    if not request._http_cache \
+            or request.method not in ['GET', 'HEAD', 'OPTIONS'] \
+            or resp.status_code > 399 \
+            or resp.is_streamed:
+        resp.cache_control.no_cache = True
+        return resp
+
+    resp.cache_control.max_age = 3600 * 6
+    # TODO: Vary
+
+    # resp.cache_control.must_revalidate = True
+    if current_user.is_authenticated():
+        resp.cache_control.private = True
+    else:
+        resp.cache_control.public = True
+    if request._http_etag is None:
+        etag_cache_keygen()
+    resp.add_etag(request._http_etag)
+    return resp
+
+
+def generate_etag(keys):
+    keys = [k + ':' + repr(v) for k, v in keys.items()]
+    return sha1('|'.join(keys)).hexdigest()
+
+
+def etag_cache_keygen(*keys):
+    if not request._http_cache:
+        return
+
+    args = sorted(set(request.args.items()))
+    # jquery where is your god now?!?
+    args = filter(lambda (k, v): k != '_', args)
+    args = [k + ':' + repr(v) for k, v in args]
+
+    keys = {
+        'args': args,
+        'user': current_user.id if current_user.is_authenticated() else None,
+        'keys': sorted(keys),
+        'lang': get_locale().language
+    }
+
+    request._http_etag = generate_etag(keys)
+    if request.if_none_match == request._http_etag:
+        raise NotModified()
--- openspending/views/context.py
+++ openspending/views/context.py
@@ -4,6 +4,7 @@ from flask.ext.login import current_user
 
 from openspending import auth
 from openspending.i18n import get_available_locales
+from openspending.views.cache import setup_caching, cache_response
 from openspending.views.home import blueprint as home
 from openspending.lib.helpers import static_path
 from openspending.lib.helpers import url_for
@@ -13,6 +14,12 @@ from openspending.lib.helpers import url_for
 def before_request():
     request._ds_available_views = []
     request._ds_view = None
+    setup_caching()
+
+
+@home.after_app_request
+def after_request(resp):
+    return cache_response(resp)
 
 
 def languages():
@@ -57,89 +64,3 @@ def template_context_processor():
         }
     }
     return data
-
-# def etag_cache_keygen(*a):
-#     """
-#     Generate ETag key for the cache.
-#     This automatically includes the username taken from the session cookie
-#     with the help of pylons
-#     """
-#     # Get the account name (authentication in pylons sets it to the
-#     # environment variable REMOTE_USER)
-#     account_name = request.environ.get('REMOTE_USER', None)
-#     etag = hashlib.sha1(repr(a) + repr(account_name)).hexdigest()
-#     etag_cache(etag)
-
-
-# def set_vary_header():
-#     """
-#     Set the vary header to force intermediate caching to be controlled by
-#     different request headers. This only sets Cookie as the value of the Vary
-#     header (because of user credentials in top navigation bar) but it could
-#     also include other request headers like Accept-Language if the site should
-#     serve different locales based on request headers.
-#     """
-#     # Set the vary header as Cookie
-#     response.vary = ['Cookie']
-
-#     # If ETag hasn't been generated we generate it
-#     if not response.etag:
-#         etag_cache_keygen()
-
-
-# class BaseController(WSGIController):
-
-#     def __call__(self, environ, start_response):
-#         """Invoke the Controller"""
-#         # WSGIController.__call__ dispatches to the Controller method
-#         # the request is routed to. This routing information is
-#         # available in environ['pylons.routes_dict']
-#         begin = time()
-#         try:
-#             return WSGIController.__call__(self, environ, start_response)
-#         finally:
-#             db.session.remove()
-#             db.session.close()
-#             log.debug("Request to %s took %sms" %
-#                       (request.path, int((time() - begin) * 1000)))
-
-#     def __before__(self, action, **params):
-#         account_name = request.environ.get('REMOTE_USER', None)
-#         if account_name:
-#             c.account = Account.by_name(account_name)
-#         else:
-#             c.account = None
-
-#         i18n.handle_request(request, c)
-
-#         c._cache_disabled = False
-#         c._must_revalidate = False
-#         c.content_section = c.dataset = None
-
-#         c.detected_l10n_languages = i18n.get_language_pairs()
-
-#     def __after__(self):
-#         db.session.close()
-#         response.pragma = None
-
-#         if not app_globals.cache_enabled or 'flash' in session._session():
-#             return
-
-#         if c._cache_disabled:
-#             return
-
-#         del response.cache_control.no_cache
-#         if len(session._session().keys()) == 2 and \
-#                 not len(request.cookies.keys()):
-#             session._current_obj().__dict__['_sess'] = None
-#             response.cache_control.public = True
-#         else:
-#             response.cache_control.private = True
-
-#         # Set vary header (this will set Cookie as a value for the vary header
-#         # so different content can be served to logged in users
-#         set_vary_header()
-
-#         response.cache_control.must_revalidate = c._must_revalidate
-#         if not c._must_revalidate:
-#             response.cache_control.max_age = 3600 * 6
