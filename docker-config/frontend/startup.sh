66339df39e2e6431f2bf4fce65efc1cb0a48e6c6
Adam Kariv <adam.kariv@gmail.com>
2016-01-21 11:34:08 +0200

    Some stability modifications
    Nginx waits for all servers to be available
    Model tables are created before os-api starts

---
+++ docker-config/frontend/startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+while ! ping -c1 os-api &>/dev/null; do :; done && echo "OS-API is UP"
+while ! ping -c1 os-conductor &>/dev/null; do :; done && echo "OS-CONDUCTOR is UP"
+while ! ping -c1 os-packager &>/dev/null; do :; done && echo "OS-PACKAGER is UP"
+
+echo "NGINX STARTING"
+nginx -g "daemon off;"
