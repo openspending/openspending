64e41bc86a3f62a356d3bfdc4fcd2317f389cce7
Adam Kariv <adam.kariv@gmail.com>
2016-05-31 07:27:59 +0300

    Fixes for redash deployment

--- docker-config/deploy.sh
+++ docker-config/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo 'now uploading:'
-for x in `docker images --format "{{.Repository}}" | grep openspending/` ; do echo $x; docker push $x ; done
+#docker save `docker images --format "{{.ID}}" openspending/redash:latest` | sudo docker-squash -t openspending/redash -verbose | docker load
+for x in `docker images --format "{{.Repository}}" | grep openspending/` ; do
+    echo $x;
+    docker push $x ;
+done
