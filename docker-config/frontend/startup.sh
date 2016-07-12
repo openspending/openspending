c087d489345c03614d5d8e72e5ded5c624af5fbf
Adam Kariv <adam.kariv@gmail.com>
2016-07-12 12:18:17 +0300

    Upgrade docker images to use alpine 3.4

--- docker-config/frontend/startup.sh
+++ docker-config/frontend/startup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+echo "HELLO WORLD!"
+
 while ! ping -c1 os-api &>/dev/null; do :; done && echo "OS-API is UP"
 while ! ping -c1 os-conductor &>/dev/null; do :; done && echo "OS-CONDUCTOR is UP"
 while ! ping -c1 os-packager &>/dev/null; do :; done && echo "OS-PACKAGER is UP"
