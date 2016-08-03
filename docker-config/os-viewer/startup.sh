4984391a5cb3230749584b843795bf2dc5c7b94f
Adam Kariv <adam.kariv@gmail.com>
2016-08-03 14:18:54 +0300

    Update OS-Viewer build with latest version

--- docker-config/os-viewer/startup.sh
+++ docker-config/os-viewer/startup.sh
@@ -11,7 +11,8 @@ if [ ! -z "$GIT_REPO" ]; then
     if [ ! -z "$GIT_BRANCH" ]; then
         git checkout origin/$GIT_BRANCH
     fi
-    cd /remote && napa eligrey/FileSaver.js:file-saver && npm install && node node_modules/.bin/gulp
+    cd /remote && napa eligrey/FileSaver.js:file-saver && napa d3/d3-plugins:d3-plugins && \
+	npm install && node node_modules/.bin/gulp
 else
     ( cd /repos/os-viewer && npm install -g napa && napa eligrey/FileSaver.js:file-saver && npm install ) || true
     ( cd /repos/os-viewer && node node_modules/gulp/bin/gulp.js ) || true
