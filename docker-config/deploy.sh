525f22082b184269ec21b7d7542c8e88ab7b41b2
Vitor Baptista <vitor@vitorbaptista.com>
2017-09-07 17:18:49 +0100

    Remove unused Docker images
    The Docker images are now configured in each separate component, so these
    aren't needed anymore.

--- docker-config/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo 'now uploading:'
-#docker save `docker images --format "{{.ID}}" openspending/redash:latest` | sudo docker-squash -t openspending/redash -verbose | docker load
-for x in `docker images --format "{{.Repository}}" | grep openspending/` ; do
-    echo $x;
-    docker push $x ;
-done
--- docker-config/os-conductor/secrets/dev-secrets/generate_key_pair.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-openssl genrsa -out .tmpkey 2048
-openssl rsa -in .tmpkey -out private.pem -outform pem
-openssl rsa -in .tmpkey -out public.pem -outform pem -pubout
-rm .tmpkey
