833572f0ca47062f9d5804d0ce9bad7d3441676e
Brook Elgie <specialbrew@gmail.com>
2018-04-23 11:26:21 +0100

    Remove os-api-loader
    As part of openspending/os-api#18 and openspending/os-conductor#28

--- Dockerfiles/os-nginx-frontend/startup.sh
+++ Dockerfiles/os-nginx-frontend/startup.sh
@@ -4,7 +4,6 @@ set -e
 echo "CHECKING DEPENDENT SERVICES!"
 
 while ! ping -c1 os-api &>/dev/null; do :; done && echo "OS-API is UP"
-while ! ping -c1 os-api-loader &>/dev/null; do :; done && echo "OS-API-LOADER is UP"
 while ! ping -c1 os-conductor &>/dev/null; do :; done && echo "OS-CONDUCTOR is UP"
 while ! ping -c1 os-packager &>/dev/null; do :; done && echo "OS-PACKAGER is UP"
 while ! ping -c1 os-viewer &>/dev/null; do :; done && echo "OS-VIEWER is UP"
